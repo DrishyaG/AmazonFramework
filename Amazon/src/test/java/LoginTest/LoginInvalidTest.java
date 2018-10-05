edff8119c4c1e0ed030facbf6171839937ef6b87
deepak <deepak@deepak-PC>
2018-10-05 16:17:09 +0530

    2 test cases updated

---
+++ Amazon/src/test/java/LoginTest/LoginInvalidTest.java
@@ -0,0 +1,8 @@
+package LoginTest;
+
+public class LoginInvalidTest {
+	public static void main(String[] args) {
+		System.out.println("2nd test case");
+	}
+
+}
---
+++ Amazon/src/test/java/LoginTest/LoginTest.java
@@ -0,0 +1,10 @@
+package LoginTest;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println("1st test case");
+	}
+}
